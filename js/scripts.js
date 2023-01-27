c8ecb716d5913afc001b3068f8a2f3fcf2156dcd
Vera Weikel <vera.weikel@gmail.com>
2023-01-27 08:54:47 -0800

    Add Pizza constructor to js.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*
+Allow the user to choose toppings and size for the pizza they'd like to order.
+Create a pizza object constructor with properties for toppings and size.
+Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
+*/
+
+function Pizza(size){
+  this.toppings = [];
+  this.size = size;
+} 
+
+
