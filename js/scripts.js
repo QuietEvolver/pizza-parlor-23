b8d12d217b1ccc61da61c3bd557af4841fe1fb4b
Vera Weikel <vera.weikel@gmail.com>
2023-01-28 05:45:38 -0800

    It will reset the project.

--- js/scripts.js
+++ js/scripts.js
@@ -9,62 +9,36 @@ function Pizza(size){
   this.size = size;
 } 
 
-// Pizza.prototype.cost = () => {
+// Create a prototype method for the cost of a pizza 
+Pizza.prototype.cost = () => {
 
+}
+
+// Pizza.prototype.details = (e) => {
+//   e.preventDefault();
+//   const form = document.querySelector("form");
+//   const log = document.querySelector("#log");
+
+//   const data = new FormData(form);
+//   let output = "";
+//   for (const entry of data) {
+//     output = `${output} ${entry[0]} = ${entry[1]}\n`;
+//     e.preventDefault();
+//   }
+//   log.innerText = output;
 // }
 
-Pizza.prototype.details = (e) => {
-  e.preventDefault();
-  const form = document.querySelector("form");
-  const log = document.querySelector("#log");
-
-  const data = new FormData(form);
-  let output = "";
-  for (const entry of data) { // 
-    //     // FIGURE OUT HOW TO CATCH EA ENTRY LOOP TO MULT
-    // if(entry[0] === "toppings"){
-    //   for( let entry of data){
-
-    //     output = `Size: ${output} ${entry[0]} = ${entry[1]}`
-    //   // 
-    //     // entry[1] = this.toppings;
-    //     // console.log("entry toppings?", entry[1]);
-    //   }
-    // } else if(entry[0] === "size"){
-    //   output = `Size: ${output} ${entry[0]} = ${entry[1]}\n`; 
-    //   // data.forEach(function (value, i) {
-    //   //     console.log('VERA %d: %s', i, value);
-    //   // });
-    // }
-    output = `${output} ${entry[0]} = ${entry[1]}\n`;
-
-    // console.log("Output: ", output);  
-    // console.log("Entry: ", entry); 
-    e.preventDefault();
-  }
-  log.innerText = output;
-  // setTimeout(output, 3000);
-  // console.log("output: ", output);    
-  // setTimeout(() => {
-  //   output
-  //   }, 500);
-
-
-}
-
-const myPizza = new Pizza(); // mock db
+// const myPizza = new Pizza(); // mock db
   
-function handleFormSubmission(e){
+// function handleFormSubmission(e){
+/// CONST RADIO AND CHECKBOXES
+//   myPizza.details(e);
 
-  myPizza.details(e);
+//   console.log("myPizza: ", myPizza);
 
-  console.log("myPizza: ", myPizza);
+//   e.preventDefault();
+// }
 
-  e.preventDefault();
-}
-
-// Create a prototype method for the cost of a pizza depending
-
-window.addEventListener("load", function(){ 
-  document.querySelector("form").addEventListener("submit", handleFormSubmission);  
-});
+// window.addEventListener("load", function(){ 
+//   document.querySelector("form").addEventListener("submit", handleFormSubmission);  
+// });
