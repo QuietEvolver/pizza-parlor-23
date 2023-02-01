6290419442ead659a2e0cf412564e3f595183081
Vera Weikel <vera.weikel@gmail.com>
2023-02-01 04:56:49 -0800

    Remove js comments.

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,6 @@ Pizza.prototype.toppingsCost = function() {
 
 Pizza.prototype.sizeCost = function() {
   let size = this.size;
-  // if(Object.keys(size)){
     switch(size){
       case("small"): 
         this.size = "small";
@@ -26,7 +25,7 @@ Pizza.prototype.sizeCost = function() {
         sizePrice = 15;
         return sizePrice;
       default: 
-    // }
+        // return MessageChannel;
   }
 }
 
@@ -34,8 +33,6 @@ Pizza.prototype.fullOrderCost = function(){
 
   let sizePrice = this.sizeCost();
   const toppingPrice = this.toppingsCost();
-  console.log("this.toppings - toppingsPrice ", toppingPrice);
-  console.log("this.sizePrice ", sizePrice);
   this.price = toppingPrice + sizePrice;
   return this.price;
 }
@@ -57,8 +54,6 @@ Pizza.prototype.details = (e) => {
   }
   log.innerText = output;
 
-  // checkbox Selection
-  //const toppings = document.querySelector("input[name='toppings']:checked").value;
   this.toppings = [];
   if(document.getElementById("mushrooms").checked===true){
     toppings.push("mushrooms");
@@ -69,41 +64,25 @@ Pizza.prototype.details = (e) => {
   if(document.getElementById("pineapple").checked===true){
     toppings.push("pineapple");
   } 
-  // toppingsArr.push(toppings); this.toppings,
+  
   myPizza.toppings = this.toppings;
-  console.log("this.toppings: ", myPizza.toppings);
   myPizza.toppingsCost(e);
-  console.log("myPizza.toppingsCost(e)", myPizza.toppingsCost(e));
-  console.log("my.topping ", myPizza.toppings);
-  /////////toppings///////////
-  // Set empty array for toppings 
-  // let toppingArr = []; 
-  // console.log("tarr", toppingArr);
-  // // Add each topping to array
-  // for (const [key, value] of data) {
-  //   if (key === "toppings") {
-  //     toppingArr.push(value);
-  //   }
-  // }
-  //////////////////// radioSelection capture(d)
+
   const size = document.querySelector("input[name='size']:checked").value;
-  myPizza.size = size;
-  console.log("myPizza.size: ", size); 
+  myPizza.size = size; 
   myPizza.sizeCost(e);
-  console.log("my.size ", myPizza.sizes);
+  console.log("my.size ", myPizza.size);
    // cost
   myPizza.fullOrderCost(e);
   console.log("myPizza.fullOrderCost(e)", myPizza.fullOrderCost(e));
-  console.log("this.price", this.price);
+  console.log("this.price", myPizza.price);
 }
 
   
 function handleFormSubmission(e){
   e.preventDefault();
-// CONST RADIO AND CHECKBOXES
   myPizza.details(e);
   console.log("myPizza: ", myPizza);
-
 }
 
 window.addEventListener("load", function(){ 
